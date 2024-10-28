603d3dc7fbf06f085ff2529849e9871bd8fb53a8
matt-wolff <mbwolff@stanford.edu>
2024-10-27 17:00:24 -0700

    Processed protein functions.

--- process_raw_data.py
+++ process_raw_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import ast
 
 RAW_DATA = "data/raw_data"
+PLOT_DIR = "data/plots"
 PROCESSED_DATA = "data/processed_data"
 COLUMNS = [
     'DB',
@@ -40,12 +42,25 @@ def main():
     data = pd.read_csv(f"{RAW_DATA}/goa_human.gaf", sep="\t", names=COLUMNS, comment="!", na_values=[""], dtype=str)
     data = data[data["Aspect"] == "F"]  # Only keep annotations detailing molecular function
     data = data[data["DB_Object_Type"] == "protein"]  #  Only keep annotations detailing proteins
-    protein_df = data.groupby("DB_Object_ID").agg({"Qualifier": list, "GO_ID": list})
-    
+    for qual in set(data["Qualifier"]):
+        print(f'{qual}: {len(data[data["Qualifier"] == qual])}')
+
+    protein_df = data.groupby("DB_Object_ID").agg({"Qualifier": list, "GO_ID": list}).reset_index()
+
+    # Make Idx columns from Id columns
+    go_id_set = set()
+    for go_id_list in protein_df["GO_ID"]:
+        for go_id in go_id_list:
+            go_id_set.add(go_id)
+    go_id_to_idx = {go_id: idx for idx, go_id in enumerate(go_id_set)}
+    protein_df["GO_Idx"] = protein_df['GO_ID'].apply(lambda x: [go_id_to_idx[go_id] for go_id in x])
+
     number_annotations = protein_df['GO_ID'].apply(len)
     max_annotations = max(number_annotations)
-    num_functions_per_protein(number_annotations, max_annotations, "data/plots/num_functions_per_protein.jpg")
-    num_functions_per_protein(number_annotations, 100, "data/plots/small_num_functions_per_protein.jpg")
+    num_functions_per_protein(number_annotations, max_annotations, f"{PLOT_DIR}/num_functions_per_protein.jpg")
+    num_functions_per_protein(number_annotations, 100, f"{PLOT_DIR}/small_num_functions_per_protein.jpg")
+
+    protein_df.to_csv(f"{PROCESSED_DATA}/protein_functions.csv")
 
 if __name__ == "__main__":
     main()
