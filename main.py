bcbfab46a9a2b5fcc7751b5cd1105db3c7eb30e1
Alec Lessing <aleclessing@stanford.edu>
2024-12-08 23:31:38 -0800

    merged

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ parser.add_argument(
 parser.add_argument(
     "--feature_dim",
     type=int,
-    default=21,
+    default=11,
     help="feature dimension",
 )
 
@@ -129,7 +129,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 loss_mse = nn.MSELoss()
 
 # DATASET_DIR = "data/processed_data/protein_inputs"
-DATASET_DIR = "data/processed_data/hdf5_files_d_20"
+DATASET_DIR = "data/processed_data/hdf5_files_d_10_2"
 
 
 def create_summary_writer(
