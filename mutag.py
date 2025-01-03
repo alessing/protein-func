6397158a4a6072ad1d4e8d5b777a59ee7d04a1f1
matt-wolff <mbwolff@stanford.edu>
2025-01-02 18:26:40 -0800

    Model in mutag.py now loaded with num_relations.

--- mutag.py
+++ mutag.py
@@ -166,7 +166,7 @@ def main(batch_size=64, lr=5e-4, dropout=0.1, weight_decay=1e-5, epochs=1000, nu
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 
-    model = GNN(num_layers=num_layers, hidden_dim=hidden_dim, dropout=dropout, lora_dim=lora_dim, num_blocks=num_blocks, heads=1, num_bases=num_bases, gnn_type=gnn_type).to(device)
+    model = GNN(num_layers=num_layers, hidden_dim=hidden_dim, dropout=dropout, num_relations=num_relations, lora_dim=lora_dim, num_blocks=num_blocks, heads=1, num_bases=num_bases, gnn_type=gnn_type).to(device)
     num_params = sum(p.numel() for p in model.parameters())
 
     # Calculate the total number of parameters in the model
