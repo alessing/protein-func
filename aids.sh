859ca6bc53e9ff59e0721801fd9889ad4ee48d8c
Sergio <sergiogcharles@gmail.com>
2025-01-03 13:01:04 -0500

    aids

---
+++ aids.sh
@@ -0,0 +1,4 @@
+python3 mutag.py --dataset AIDS --epochs 100
+python3 mutag.py --dataset AIDS --epochs 100 --lora_dim 2
+python3 mutag.py --dataset AIDS --epochs 100 --num_blocks 4
+python3 mutag.py --dataset AIDS --epochs 100 --num_bases 1
